5b2208ed1e7e8235a70a629164509c44f546f2d8
Stephan The God <teig.stephan@gmail.com>
2023-05-15 11:17:05 +0200

    newsletter start

---
+++ newsletter.php
@@ -0,0 +1,20 @@
+<html><head>
+    <title>CIS Times</title>
+    <link rel="icon" type="image/x-icon" href="img/favicon.ico">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+<br><br><br><br><br><br>
+
+
+
+<footer>
+    <a href="https://cissdesign.no/cistimes" target="_blank" style="color: whitesmoke;">CISSDESIGN.NO/CISTIMES</a>
+    <br>
+    <p style="font-size: medium; color: white;">© 2023 Stephan Teig</p>
+  </footer>
+
+</body></html>
