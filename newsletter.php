fb9c646a61f4ed8713c2e4ad2a36c8ad672a63b7
Stephan The God <teig.stephan@gmail.com>
2023-05-19 09:29:32 +0200

    newsletter

--- newsletter.php
+++ newsletter.php
@@ -9,7 +9,7 @@
 <?php include 'header.php'; ?>
 <br><br><br><br><br><br>
 
-
+<iframe width="540" height="305" src="https://6575bd18.sibforms.com/serve/MUIEAFk5SxGtOYRHlSWQjzIDXMoM_46w8D3JCRF7_A_7r5NayFpeg3f5p8GhcvFGqeZfyFg7WD0ktRDvk2xnxmIx2o0f6cE2B1z1Dy2FRydFHgbjrLDTUtC7dU7mRHJiZTJcVM2-R6i4kZCFSH-gKk0YUtD9KOpEixHqhhGTzwz-QadqAO1HwvJXLWB0YOSrAmg5Wzo-wRx6X3YR" frameborder="0" scrolling="auto" allowfullscreen style="display: block;margin-left: auto;margin-right: auto;max-width: 100%;"></iframe>
 
 <footer>
     <a href="https://cissdesign.no/cistimes" target="_blank" style="color: whitesmoke;">CISSDESIGN.NO/CISTIMES</a>
