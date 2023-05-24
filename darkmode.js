bb0a6ed98a79af8bb9bc8548ea90e639a4ffc7c2
Stephan The God <teig.stephan@gmail.com>
2023-05-24 13:46:53 +0200

    darkmode.main

---
+++ darkmode.js
@@ -0,0 +1,25 @@
+
+
+
+function addDarkmodeWidget() {
+    new Darkmode().showWidget();
+  }
+
+  const options = {
+  bottom: '120px', // default: '32px'
+  right: 'unset', // default: '32px'
+  left: '32px', // default: 'unset'
+  time: '0.5s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: true, // default: true,
+  label: '🌓', // default: ''
+  autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
+
+  window.addEventListener('load', addDarkmodeWidget);
--- index.php
+++ index.php
@@ -5,6 +5,10 @@
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+
+<script src="https://cdn.jsdelivr.net/npm/darkmode-js@1.5.7/lib/darkmode-js.min.js"></script>
+<script src="darkmode.js"></script>
+
    
 <?php include 'header.php'; ?>
 <br><br><br><br><br><br>
